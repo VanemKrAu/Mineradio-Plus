b41e665669e25f6ea1a3e7579bb36f51451b07f6
VanemKrAu <vanemmailbox@126.com>
2026-07-08 21:57:22 +0800

    PKG纹理：去掉generateMipmap，改用LINEAR过滤(兼容非2幂尺寸)

--- public/js/app.js
+++ public/js/app.js
@@ -25193,10 +25193,10 @@ function loadPkgBgTexture(name, url) {
       gl.bindTexture(gl.TEXTURE_2D, tex);
       gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
       gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
-      gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR_MIPMAP_LINEAR);
+      gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
       gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
       gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, img);
-      gl.generateMipmap(gl.TEXTURE_2D);
+      // 不 generateMipmap：壁纸纹理多为非2的幂尺寸，会报 GL_INVALID_OPERATION
       gl.bindTexture(gl.TEXTURE_2D, null);
       pkgBg.textures[name] = tex;
       ok(tex);
